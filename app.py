fcd2069dd440f945e13be8385825faf2e37e5a81
PaulaCardenas2 <paulacardenas153@gmail.com>
2025-05-23 22:37:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
-import streamlit as st
+pdf_file = crear_reporte_pdf(costo_fijo, costo_variable, precio_venta, punto_eq, max_cantidad)
 
-# Título de la app
-st.title("Descarga tu PDF")
-
-# Ruta o archivo PDF (puedes cambiar el nombre si es otro)
-pdf_file = "documento.pdf"
-
-# Abrir el PDF en modo binario
 with open(pdf_file, "rb") as file:
-    pdf_bytes = file.read()
-
-# Botón para descargar el PDF
-st.download_button(
-    label="Descargar PDF",
-    data=pdf_bytes,
-    file_name="documento_descargado.pdf",
-    mime="application/pdf"
-)
-
-st.write("Haz clic en el botón para descargar el PDF.")
+    st.download_button(
+        label="Descargar reporte PDF",
+        data=file,
+        file_name=pdf_file,
+        mime="application/pdf"
+    )

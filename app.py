4cb175f1fb7ce10669937e6bb39cd1083e873a25
PaulaCardenas2 <paulacardenas153@gmail.com>
2025-05-23 22:41:43 -0500

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-pdf_file = crear_reporte_pdf(costo_fijo, costo_variable, precio_venta, punto_eq, max_cantidad)
-
-with open(pdf_file, "rb") as file:
-    st.download_button(
-        label="Descargar reporte PDF",
-        data=file,
-        file_name=pdf_file,
-        mime="application/pdf"
-    )
